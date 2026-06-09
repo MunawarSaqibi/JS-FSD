bc54577187b722c4ae461368d669c100f741aa09
MunawarSaqibi <mohdmunawar.mar@gmail.com>
2026-06-09 15:21:31 +0530

    Assignment added

---
+++ JavaScript-Day-1/Assignment.js
@@ -0,0 +1,62 @@
+// Problem 1
+// let firstName = "Munawar";
+// let age = 21;
+// let isStudent = true;
+// let favColor = "Blue";
+// let siblings = 2;
+// console.log(firstName)
+// console.log(age)
+
+
+// Problem 2
+// console.log(typeof age)
+// console.log(typeof firstName)
+// console.log(typeof isStudent)
+// console.log(typeof favColor)
+// console.log(typeof siblings)
+
+
+// Problem 3
+// let score;
+// console.log(score)
+// console.log(typeof score)
+// let winner = null;
+// console.log(winner)
+// console.log(typeof winner)
+
+// Undefined is when we forget to assign vale
+// whereas Null is assigned when we intentionally don't assign value
+
+// Problem 4
+// let firstName = "Mohd";
+// let lastName = "Munawar"
+// let fullName = "Mohd Munawar"
+// console.log(fullName)
+
+// Problem 5
+// let a = "5";
+// let b = 3;
+// let result = a + b;
+// console.log(result);
+// console.log(typeof result);
+
+//prediction , a+b=8 
+// output , a+b=53
+//Numbers will not be added as "a" is a string ,rather a+b will be concatenated
+
+//Part B
+
+// problem 6:Basic Calculator
+
+ let num1=45;
+ let num2=12;
+
+console.log("Sum is", num1+num2)
+console.log("Difference is" ,num1-num2)
+console.log("Product is" , num1*num2)
+console.log("Quotient is", num1/num2)
+console.log("Remainder is", num1%num2)
+
+
+
+
--- JavaScript-Day-1/script.js
+++
@@ -1,2 +0,0 @@
-let age=31;
-console.log(age)
